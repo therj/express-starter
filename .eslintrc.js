90acbafbc5d0beab6c49642c919a8ada7bfebf3f
Rabindra Joshi <mail@rjoshi.net>
2020-03-09 16:43:29 +0545

    more linting rules
    - Allow console logs
    - Use backtick

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,38 @@ module.exports = {
         "ecmaVersion": 2020
     },
     "rules": {
-        "prettier/prettier": "error",
-        "quotes": "single"
+        "prettier/prettier": [
+            "error",
+            {
+                "trailingComma": "es5",
+                "semi": true,
+                "printWidth": 80
+            }
+        ],
+        "comma-dangle": [
+            "error",
+            {
+                "arrays": "always-multiline",
+                "objects": "always-multiline",
+                "imports": "never",
+                "exports": "never",
+                "functions": "ignore"
+            }
+        ],
+        "no-param-reassign": [
+            "error",
+            {
+                "props": false
+            }
+        ],
+        "no-console": "off",
+        "quotes": [
+            "error",
+            "backtick",
+            {
+                "avoidEscape": true,
+                "allowTemplateLiterals": false
+            }
+        ],
     }
 };
